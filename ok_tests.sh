b00253184acf542694f97cb3dbe668ce29de7284
moritz.lenz <moritz.lenz@590fedd0-9619-0410-982d-5f90ca37e7c8>
2006-07-27 21:49:33 +0000

    - Added test cases - Clarification in README about the project page

---
+++ ok_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd ok_tests
+for i in * ;
+do
+	if [ -e  ../ok_test_ref/"$i" ] ; then
+		../yasss < "$i" | diff - ../ok_test_ref/"$i";
+		if [ $? != "0" ]; then 
+			echo -n 'output of `../yasss` < '"$i"' and its refernce'
+			echo " file differ!"
+			exit 1
+		fi
+	fi
+done
